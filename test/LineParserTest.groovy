e685bba8ca25e59f1376b571427818e57d72ce61
gianninasd <angelone55@hotmail.com>
2018-12-29 08:53:49 -0500

    made test more groovy like

--- test/LineParserTest.groovy
+++ test/LineParserTest.groovy
@@ -8,111 +8,66 @@ class LineParserTest extends GroovyTestCase {
   def parser = new LineParser()
 
   void testGoodLine() {
-    try {
-      parser.parse(0,'jim7025,1500,4111111111111111,10,2020,Rick,Hunter,rick@sdf3.com,M5H 2N2')
-    }
-    catch( Exception ex ) {
-      throw new Exception('Was not excepting it to raise an Exception', ex)
-    }
+    parser.parse(0,'jim7025,1500,4111111111111111,10,2020,Rick,Hunter,rick@sdf3.com,M5H 2N2')
   }
 
   void testMissingTokens() {
-    try {
+    shouldFail MissingTokensException, {
       parser.parse(0,'jim7025,1500,4111111111111111')
-      throw new Exception('Was excepting it to raise MissingTokensException')
-    }
-    catch( MissingTokensException ex ) {
-      // pass
     }
   }
 
   void testMissingRef() {
-    try {
+    shouldFail IllegalArgumentException, {
       parser.parse(0,',1500,4111111111111111,10,2020,Rick,Hunter,rick@sdf3.com,M5H 2N2')
-      throw new Exception('Was excepting it to raise ValueError')
     }
-    catch( IllegalArgumentException ex ) {
-      // pass
-    }    
   }
 
   void testBadAmount() {
-    try {
+    shouldFail IllegalArgumentException, {
       parser.parse(0,'jim22,xx,4111111111111111,10,2020,Rick,Hunter,rick@sdf3.com,M5H 2N2')
-      throw new Exception('Was excepting it to raise ValueError')
-    }
-    catch( IllegalArgumentException ex ) {
-      // pass
     }    
   }
 
   void testBadCardNbr() {
-    try {
+    shouldFail IllegalArgumentException, {
       parser.parse(0,'jim22,1500,xxxxxx,10,2020,Rick,Hunter,rick@sdf3.com,M5H 2N2')
-      throw new Exception('Was excepting it to raise ValueError')
-    }
-    catch( IllegalArgumentException ex ) {
-      // pass
     }
   }
 
   void testBadCardExpMth() {
-    try {
+    shouldFail IllegalArgumentException, {
       parser.parse(0,'jim22,1500,4111111111111111,15,2020,Rick,Hunter,rick@sdf3.com,M5H 2N2')
-      throw new Exception('Was excepting it to raise ValueError')
-    }
-    catch( IllegalArgumentException ex ) {
-      // pass
     }
   }
 
   void testBadCardExpYear() {
-    try {
+    shouldFail IllegalArgumentException, {
       parser.parse(0,'jim22,1500,4111111111111111,10,1995,Rick,Hunter,rick@sdf3.com,M5H 2N2')
-      throw new Exception('Was excepting it to raise ValueError')
-    }
-    catch( IllegalArgumentException ex ) {
-      // pass
     }
   }
 
   void testBadFirstName() {
-    try {
+    shouldFail IllegalArgumentException, {
       parser.parse(0,'jim22,1500,4111111111111111,10,2020,,Hunter,rick@sdf3.com,M5H 2N2')
-      throw new Exception('Was excepting it to raise ValueError')
-    }
-    catch( IllegalArgumentException ex ) {
-      // pass
     }
   }
 
   void testBadLastName() {
-    try {
+    shouldFail IllegalArgumentException, {
       parser.parse(0,'jim22,1500,4111111111111111,10,2020,Rick,,rick@sdf3.com,M5H 2N2')
-      throw new Exception('Was excepting it to raise ValueError')
-    }
-    catch( IllegalArgumentException ex ) {
-      // pass
     }
   }
 
   void testBadEmail() {
-    try {
+    shouldFail IllegalArgumentException, {
       parser.parse(0,'jim22,1500,4111111111111111,10,2020,Rick,Hunter,,M5H 2N2')
-      throw new Exception('Was excepting it to raise ValueError')
-    }
-    catch( IllegalArgumentException ex ) {
-      // pass
     }
   }
 
   void testBadZipCode() {
-    try {
+    shouldFail IllegalArgumentException, {
       parser.parse(0,'jim22,1500,4111111111111111,10,2020,Rick,Hunter,rick@sdf3.com,!!')
-      throw new Exception('Was excepting it to raise ValueError')
-    }
-    catch( IllegalArgumentException ex ) {
-      // pass
     }
   }
 }
